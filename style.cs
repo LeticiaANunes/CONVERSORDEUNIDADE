564386da7f8205c3a58828f37a8a113cffe595c2
Leticia A Nunes <133019280+LeticiaANunes@users.noreply.github.com>
2023-07-13 14:40:41 -0300

    Create style.cs
    Foi acrescentado a parte de estilização na página

---
+++ style.cs
@@ -0,0 +1,56 @@
+/* Estilização geral da tabela */
+table {
+    width: 100%;
+    border-collapse: collapse;
+    background-color: #f2f2f2;
+    border: 1px solid #ccc;
+    margin-bottom: 20px;
+}
+
+/* Estilos para as células de cabeçalho */
+th {
+    background-color: #333;
+    color: #fff;
+    text-align: center;
+    padding: 10px;
+}
+
+/* Estilos para as células de dados */
+td {
+    padding: 8px;
+    text-align: left;
+}
+
+/* Estilos para a primeira célula de cada linha */
+td:first-child {
+    font-weight: bold;
+}
+
+/* Estilos para as células de entrada (inputs e selects) */
+input[type="text"],
+select {
+    padding: 5px;
+    width: 150px;
+}
+
+/* Estilos para o botão */
+button {
+    padding: 10px;
+    background-color: #333;
+    color: #fff;
+    border: none;
+    cursor: pointer;
+}
+
+/* Estilos para o resultado */
+#resultado {
+    font-weight: bold;
+    margin-top: 10px;
+}
+
+
+body {
+    background-image: url("../img/Dorama\ imagem\ de\ fundo\ para\ celular.png");
+    background-repeat: no-repeat;
+    background-size: cover;
+}
